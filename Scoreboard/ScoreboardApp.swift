a430b703085846cee746c4f72b3835dd430bcb5d
Robert Bennici <rbnyc1@gmail.com>
2025-02-20 22:47:28 -0500

    Initial Commit

---
+++ Scoreboard/ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreboardApp.swift
+//  Scoreboard
+//
+//  Created by Grey Wolf on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
